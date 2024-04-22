3c68578024b509b5ca378b715886d91c2efe0600
Selingrová Klára (257041) <257041@vutbr.cz>
2024-04-22 12:12:35 +0200

    fibonacci

--- fibo.py
+++ fibo.py
@@ -1,5 +1,15 @@
+def recursive_nth_fibo(n):
+    if n == 0:
+        return 0
+    elif n == 1:
+        return 1
+    else:
+        return recursive_nth_fibo(n - 1) + recursive_nth_fibo(n - 2)
+
 
 def main():
+    k = recursive_nth_fibo(5)
+    print(k)
     pass
 
 
